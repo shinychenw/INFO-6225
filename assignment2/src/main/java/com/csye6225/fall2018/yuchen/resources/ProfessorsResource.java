ab8ab7e23c713c828279c48c51bfc41f36bba0cd
shinychenw <wang.yuchen3@husky.neu.edu>
2018-11-14 00:15:32 -0800

    Assignment2 Creation with Professor Class finished.

---
+++ assignment2/src/main/java/com/csye6225/fall2018/yuchen/resources/ProfessorsResource.java
@@ -0,0 +1,56 @@
+package com.csye6225.fall2018.yuchen.resources;
+
+import com.csye6225.fall2018.yuchen.datamodel.Professor;
+import com.csye6225.fall2018.yuchen.service.ProfessorsService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Iterator;
+import java.util.List;
+
+//../webapi/myresource
+@Path("professors")
+public class ProfessorsResource {
+
+    ProfessorsService professorsService = new ProfessorsService();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Professor> getProfessorsByDepartment(@QueryParam("department") String department){
+        if(department == null){
+            return professorsService.getAllProfessors();
+        }
+        return professorsService.getProfessorsByDepartment(department);
+    }
+
+    // ... webapi/professor/1
+    @GET
+    @Path("/{professorId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Professor getProfessor(@PathParam("professorId") String profId){
+        return professorsService.getProfessor(profId);
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Professor addProfessor(Professor prof){
+        return  professorsService.addProfessor(prof);
+    }
+
+    @DELETE
+    @Path("/{professorId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Professor deleteProfessor(@PathParam("professorId") String profId){
+        return professorsService.deleteProfessor(profId);
+    }
+
+    @PUT
+    @Path("/{professorId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Professor updateProfessor(@PathParam("professorId") String profId, Professor prof){
+        return professorsService.updateProfessorInformation(profId, prof);
+    }
+}
---
+++ assignment2/src/main/java/com/csye6225/fall2018/yuchen/service/ProfessorsService.java
@@ -0,0 +1,109 @@
+package com.csye6225.fall2018.yuchen.service;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.csye6225.fall2018.yuchen.datamodel.DynamoDBConnector;
+import com.csye6225.fall2018.yuchen.datamodel.Professor;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class ProfessorsService {
+
+    static DynamoDBConnector dynamoDB;
+    DynamoDBMapper mapper;
+
+    public ProfessorsService() {
+        dynamoDB = new DynamoDBConnector();
+        dynamoDB.init();
+        mapper = new DynamoDBMapper(dynamoDB.getClient());
+    }
+
+    //Adding a professor
+    public Professor addProfessor(Professor prof){
+
+        mapper.save(prof);
+        return prof;
+    }
+
+    //Deleting
+    public Professor deleteProfessor(String profId){
+        Map<String, AttributeValue> eav = new HashMap<>();
+        eav.put(":val1", new AttributeValue().withS(profId));
+
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("ProfessorId = :val1").withExpressionAttributeValues(eav);
+
+        Iterator<Professor> scanResult = mapper.scan(Professor.class, scanExpression).iterator();
+
+        Professor prof =  scanResult.next();
+        mapper.delete(prof);
+        return prof;
+    }
+
+    //Updating
+    public Professor updateProfessorInformation(String profId, Professor prof){
+        Map<String, AttributeValue> eav = new HashMap<>();
+        eav.put(":val1", new AttributeValue().withS(profId));
+
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("ProfessorId = :val1").withExpressionAttributeValues(eav);
+
+        Iterator<Professor> scanResult = mapper.scan(Professor.class, scanExpression).iterator();
+
+        Professor oldProfObject =  scanResult.next();
+        String Id = oldProfObject.getId();
+        profId = oldProfObject.getProfessorId();
+        prof.setId(Id);
+        prof.setProfessorId(profId);
+
+        mapper.save(prof);
+
+        return prof;
+    }
+
+    // Getting a list of all professor
+    // GET "..webapi/professors"
+    public List<Professor> getAllProfessors() {
+        //Getting the list
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withIndexName("ProfessorId")
+                .withConsistentRead(false);
+
+        List<Professor> ilist = mapper.scan(Professor.class, scanExpression);
+        //Iterator<Professor> indexItems = ilist.iterator();
+        return ilist;
+    }
+
+    //Getting a professor
+    public Professor getProfessor(String profId){
+
+        Map<String, AttributeValue> eav = new HashMap<>();
+        eav.put(":val1", new AttributeValue().withS(profId));
+
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("ProfessorId = :val1").withExpressionAttributeValues(eav);
+
+        Iterator<Professor> scanResult = mapper.scan(Professor.class, scanExpression).iterator();
+
+        return scanResult.next();
+
+        /*Professor prof = mapper.load(Professor.class, profId);
+        return prof;*/
+    }
+
+    //Get professors by department
+    public List<Professor> getProfessorsByDepartment(String department){
+        Map<String, AttributeValue> eav = new HashMap<>();
+        eav.put(":val1", new AttributeValue().withS(department));
+
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("department = :val1").withExpressionAttributeValues(eav);
+
+        List<Professor> scanResult = mapper.scan(Professor.class, scanExpression);
+        return scanResult;
+    }
+}
